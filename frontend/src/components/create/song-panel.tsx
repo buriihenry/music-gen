7160f2be1e8ddba811787a6cb12f2f483e47806a
buriihenry <buriihenry@gmail.com>
2025-08-08 17:00:40 +0300

    feat: add song-panel

--- frontend/src/components/create/song-panel.tsx
+++ frontend/src/components/create/song-panel.tsx
@@ -7,10 +7,31 @@ import { Button } from "../ui/button";
 import { Plus } from "lucide-react";
 import { Switch } from "../ui/switch";
 
+const inspirationTags = [
+  "80s synth-pop",
+  "Acoustic ballad",
+  "Epic movie score",
+  "Lo-fi hip hop",
+  "Driving rock anthem",
+  "Summer beach vibe",
+];
+
 export function SOngPanel(){
     const [mode, setMode] = useState <"simple" | "custom">("simple");
     const [description, setDescription] = useState("");
     const [Instrumental, setInstrumental] = useState(false);
+
+    const handleInspirationTagClick = (tag: string) => {
+        const currentTags = description.split(",").map((s) =>s.trim()).filter((s) => s);
+        if(!currentTags.includes(tag)){
+            if(description.trim() ===""){
+                setDescription(tag)
+            }else{
+                setDescription(description + ", " + tag);
+            }
+        }
+    };
+
 
 
 
@@ -31,6 +52,7 @@ export function SOngPanel(){
                             <div className="flex flex-col gap-3">
                                 <label className="text-sm font-medium">
                                     Describe your Song
+                                    </label>
                                     <Textarea 
                                     value={description}
                                     onChange = {(e) => setDescription(e.target.value)}
@@ -38,7 +60,7 @@ export function SOngPanel(){
                                     className="min-h-[120px]"
                                     />
 
-                                </label>
+                                
                             </div>
                             <div className="flex items-center justify-between">
                                 <Button 
@@ -58,7 +80,50 @@ export function SOngPanel(){
                                 </div>
                                 
                             </div>
+                            <div className="flex flex-col gap-3">
+                                <label className="text-sm font-medium">
+                                    Inspiration
+                                </label>
+                                <div className="w-full overflow-x-auto whitespace-nowrap">
+                                    <div className="flex gap-2 pb-2">
+                                        {inspirationTags.map((tag) =>(
+                                            <Button 
+                                            variant="outline"
+                                            key={tag}
+                                            size="sm"
+                                            className="h-7 flex-shrink-0 bg-transparent text-xs"
+                                            onClick={() => handleInspirationTagClick(tag)}
+
+                                            
+                                            >
+                                                <Plus className="mr-1"/>
+                                                {tag}
+                                            </Button>
+                                        ))}
+
+                                    </div>
+
+                                </div>
+                            </div>
                         </TabsContent>
+                        <TabsContent value="custom" className="mt-6 space-y-6">
+                            <div className="flex flex-col gap-3">
+                                <div className="flex items-center justify-between">
+                                    <label className="text-sm font-medium">
+                                    Lyrics
+                                    </label>
+                                    <div className="flex items-center gap-1">
+                                        <Button 
+                                        size="sm" 
+                                        className="h-7 text-xs"> 
+                                        Auto
+
+                                        </Button>
+
+                                    </div>
+                                </div>
+                                    </div>
+                                    </TabsContent>
                     </Tabs>
 
 
