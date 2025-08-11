810f5ae161b43dd46b544deee783d1ebb2947a21
buriihenry <buriihenry@gmail.com>
2025-08-11 16:58:59 +0300

    generate song feat

--- frontend/src/actions/generation.ts
+++ frontend/src/actions/generation.ts
@@ -1,23 +1,60 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { aw } from "node_modules/better-auth/dist/shared/better-auth.CUMpWXN6";
 import { inngest } from "~/inngest/client";
 import { auth } from "~/lib/auth"
 import { db } from "~/server/db";
 
+export interface GenerateRequest{
+  prompt? : string;
+  lyrics? : string;
+  fullDescribedSong? : string;
+  describedLyrics? : string;
+  instrumental? : boolean;
+}
 
-export async function queueSong() {
-    const session = await auth.api.getSession({
+export async function generateSong(generateRequest: GenerateRequest) {
+
+  const session = await auth.api.getSession({
         headers: await headers(),
     });
     if(!session) redirect("/auth/sign-in");
+
+    await queueSong(generateRequest, 7.5, session.user.id);
+    await queueSong(generateRequest, 15, session.user.id);
+
+    revalidatePath("/create");
     
+
+}
+
+
+export async function queueSong(
+  generateRequest:GenerateRequest,
+  guidanceScale: number,
+  userId: string,
+) {
+  
+  let title="untitled"
+  if(generateRequest.describedLyrics) title = generateRequest.describedLyrics
+  
+  if (generateRequest.fullDescribedSong) title = generateRequest.fullDescribedSong
+
+  title = title.charAt(0).toLocaleUpperCase() + title.slice(1);
+
   const song = await db.song.create({
     data:{
-        userId: session.user.id,
-        title: "Test song 1",
-        fullDescribedSong: "Hip hop song"
+        userId: userId,
+        title: title,
+        prompt: generateRequest.prompt,
+        fullDescribedSong: generateRequest.fullDescribedSong,
+        instrumental: generateRequest.instrumental,
+        guidanceScale: guidanceScale,
+        audioDuration: 180
+
 
     },
   });
