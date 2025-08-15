714fd35d7bbcc1bd42e8f6f9e3f4c0dfb36d2b8c
buriihenry <buriihenry@gmail.com>
2025-08-15 14:13:19 +0300

    customer portal

---
+++ frontend/src/app/(main)/customer-portal/page.tsx
@@ -0,0 +1,13 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "~/lib/auth";
+
+
+export default async function HomePage() {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session){
+        redirect("/auth/sign-in");
+    }
+}
